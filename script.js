929b8f8c56da9c1ef3556db57ced3d69d2b70ebc
Fifla47 <164636161+Fifla47@users.noreply.github.com>
2024-05-27 14:09:55 +0200

    Added the API

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formateDate(date) {
@@ -41,7 +43,7 @@ function formateDate(date) {
 
 function searchCity(city) {
   let apiKey = "40970o8b11f25c36fd4a6e41693190bt";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -52,8 +54,14 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
+function getForecast(city) {
+  let apiKey = "40970o8b11f25c36fd4a6e41693190bt";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
 
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
@@ -67,14 +75,15 @@ function displayForecast() {
         <div class="weather-forecast-icon">⛅</div>
         <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-                <strong>15°</strong>
+                <strong> 15° </strong>
             </div>
-            <div class="weather-forecast-temperature">9°</div>
+            <div class="weather-forecast-temperature"> 9° </div>
         </div>
     </div>
 `;
   });
 
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
@@ -82,4 +91,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Johannesburg");
-displayForecast();
